b937722f53e7971ca7717944aff691ea5a9bd27e
David Rozas <david.rozas@gmail.com>
2016-11-07 12:44:29 +0100

    Changes for new PHP version on gD

--- web-scripts/rss_to_db_cron.php
+++ web-scripts/rss_to_db_cron.php
@@ -22,33 +22,37 @@
  *
  */
 
-$db_hostname = "yourdatabaseserverhere";
-$db_username = "yourusernamehere";
-$db_password = "yourpasswordhere";
+// Credentials
+$db_hostname = "localhost";
+$db_username = "XXXX";
+$db_password = "XXXX";
+$db_database = "XXXX";
+$private_access_key="XXXX";
 
-//Setting default timezone
+// Variables for tables
+$tbl_name="executions"; // Table name
+$tbl_entries = "rssingest";
+
+//Setting timezone
 date_default_timezone_set('Europe/Madrid');
 
-
-// Variables for counter
-$tbl_name = "executions"; // Table name 
-
 // In this version there are no params: url is hardcoded and no need for key
-
 $feed_url = "https://www.drupal.org/planet/rss.xml";
 
 
 try
-{
-	// Query the database
-	$db = mysql_connect($db_hostname,$db_username,$db_password);
-	if (!$db)
 	{
-		die("Could not connect: " . mysql_error());
+	
+	$mysqli = new mysqli($db_hostname, $db_username, $db_password, $db_database);
+	
+	// Check connection
+	if ($mysqli->connect_errno) {
+		printf("Connect failed: %s\n", $mysqli->connect_error);
+		exit();
 	}
-	mysql_select_db("drupalrss", $db);
-
-
+	
+	echo "Starting to work with feed URL '" . $feed_url . "'";
+	
 	libxml_use_internal_errors(true);
 	$RSS_DOC = simpleXML_load_file($feed_url);
 	if (!$RSS_DOC) {
@@ -57,57 +61,48 @@ try
 			echo "\t", $error->message;
 		}
 	}
-
-
+	
 	// Get title, link, managing editor, and copyright from the document
 	$rss_title = $RSS_DOC->channel->title;
 	$rss_link = $RSS_DOC->channel->link;
 	$rss_editor = $RSS_DOC->channel->managingEditor;
 	$rss_copyright = $RSS_DOC->channel->copyright;
 	$rss_date = $RSS_DOC->channel->pubDate;
-
+	
 	// Loop through each item in the RSS document
 	foreach($RSS_DOC->channel->item as $RSSitem)
 	{
-
+	
 		$item_id 	= md5($RSSitem->title);
-		$fetch_date = date("Y-m-j G:i:s"); //NOTE: we don't use a DB SQL function so its database independant
+		$fetch_date = date("Y-m-j G:i:s"); //NOTE: we don't use a DB SQL function so its database independent
 		$item_title = $RSSitem->title;
 		$item_date  = date("Y-m-j G:i:s", strtotime($RSSitem->pubDate));
 		$item_url	= $RSSitem->link;
-
+	
+		echo "Processing item '" , $item_id , "' on " , $fetch_date 	, "<br/>";
+		echo $item_title, " - ";
+		echo $item_date, "<br/>";
+		echo $item_url, "<br/>";
+	
 		// Insert the item only if it does not exist already (based on hash key)
-		$item_exists_sql = "SELECT item_id FROM rssingest where item_id = '" . $item_id . "'";
-		$item_exists = mysql_query($item_exists_sql, $db);
-		if(mysql_num_rows($item_exists)<1)
+		$item_exists_sql = "SELECT item_id FROM " . $tbl_entries . " where item_id = '" . $item_id . "'";
+		echo "Exec " . $item_exists_sql ."<br/>";
+		//$item_exists = mysql_num_rows (mysql_query($item_exists_sql, $db));
+		if(mysqli_num_rows($mysqli->query($item_exists_sql)) >=1)
 		{
-			$item_insert_sql = "INSERT INTO rssingest(item_id, feed_url, item_title, item_date, item_url, fetch_date) VALUES ('" . $item_id . "', '" . $feed_url . "', '" . $item_title . "', '" . $item_date . "', '" . $item_url . "', '" . $fetch_date . "')";
-			$insert_item = mysql_query($item_insert_sql, $db);
+			echo "<font color=blue>Not inserting existing item..</font><br/>";
+		}else{
+			echo "<font color=green>Inserting new item..</font><br/>";
+			$item_insert_sql = "INSERT INTO " . $tbl_entries . "(item_id, feed_url, item_title, item_date, item_url, fetch_date) VALUES ('" . $item_id . "', '" . $feed_url . "', '" . $item_title . "', '" . $item_date . "', '" . $item_url . "', '" . $fetch_date . "')";
+			echo "Exec " . $item_insert_sql ."<br/>";
+			$res = $mysqli->query($item_insert_sql);
+			echo "Exec " . $res ."<br/>";
 		}
-	}
-
-
-	//Select counter
-	$sql = "SELECT * FROM $tbl_name";
-	$result = mysql_query($sql);
-
-	$rows = mysql_fetch_array($result);
-	$counter = $rows['counter'];
-
-	// If there is not value in the counter, set it to 1
-	if(empty($counter)){
-		$counter = 1;
-		$sql1 = "INSERT INTO $tbl_name(counter) VALUES('$counter')";
-		$result1 = mysql_query($sql1);
-	}
-
-	// Increase the counter
-	$addcounter = $counter+1;
-	$sql2 = "UPDATE $tbl_name SET counter='$addcounter'";
-	$result2 = mysql_query($sql2);
-
-} catch (Exception $e)
-{
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
+		echo "<br/>";
+	}	
+	
+	$mysqli->close();
+} catch (Exception $e) {
+	echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
 ?>
--- web-scripts/rss_to_db_online.php
+++ web-scripts/rss_to_db_online.php
@@ -22,55 +22,53 @@
  *
  */
 
-$db_hostname = "yourdatabaseserverhere";
-$db_username = "yourusernamehere";
-$db_password = "yourpasswordhere";
+// Credentials
+$db_hostname = "localhost";
+$db_username = "XXXX";
+$db_password = "XXXX";
+$db_database = "XXXX";
+$private_access_key="XXXX";
 
-$private_access_key="yourprivateaccesskeyhere";
+// Variables for tables
+$tbl_name="executions"; // Table name
+$tbl_entries = "rssingest";
 
-// Variables for counter
-$tbl_name="executions"; // Table name 
+//Setting timezone
+date_default_timezone_set('Europe/Madrid');
 
-// Check a few bits and pieces
 
-if(isset($_GET['feed_url']))
-{
+
+// Check get parameters
+if(isset($_GET['feed_url'])) {
 	$feed_url = $_GET['feed_url'];
 }else{
 	die("Need to pass the (consistent) 'feed url'");
 }
 
 
-if(isset($_GET['access_key']))
-{
-
-	if($_GET['access_key']==$private_access_key)
-	{
+if(isset($_GET['access_key'])) {
+	if($_GET['access_key']==$private_access_key) {
 		echo "Access key correct, proceeding...<br/><br/>";
 	}else{
 		die("wrong access key");
 	}
-}
-else{
+}else{
 	die("Need to pass the 'access_key' URL parameter");
 }
 
-
 try
-{
-	// Query the database
-	$db = mysql_connect($db_hostname,$db_username,$db_password);
-	if (!$db)
 	{
-		die("Could not connect: " . mysql_error());
+	
+	$mysqli = new mysqli($db_hostname, $db_username, $db_password, $db_database);
+	
+	// Check connection
+	if ($mysqli->connect_errno) {
+		printf("Connect failed: %s\n", $mysqli->connect_error);
+		exit();
 	}
-	mysql_select_db("drupalrss", $db);
-
+	
 	echo "Starting to work with feed URL '" . $feed_url . "'";
-
-	/* Parse XML from  http://www.instapaper.com/starred/rss/580483/qU7TKdkHYNmcjNJQSMH1QODLc */
-	//$RSS_DOC = simpleXML_load_file('http://www.instapaper.com/starred/rss/580483/qU7TKdkHYNmcjNJQSMH1QODLc');
-
+	
 	libxml_use_internal_errors(true);
 	$RSS_DOC = simpleXML_load_file($feed_url);
 	if (!$RSS_DOC) {
@@ -79,67 +77,48 @@ try
 			echo "\t", $error->message;
 		}
 	}
-
-
+	
 	// Get title, link, managing editor, and copyright from the document
 	$rss_title = $RSS_DOC->channel->title;
 	$rss_link = $RSS_DOC->channel->link;
 	$rss_editor = $RSS_DOC->channel->managingEditor;
 	$rss_copyright = $RSS_DOC->channel->copyright;
 	$rss_date = $RSS_DOC->channel->pubDate;
-
+	
 	// Loop through each item in the RSS document
 	foreach($RSS_DOC->channel->item as $RSSitem)
 	{
-
+	
 		$item_id 	= md5($RSSitem->title);
-		$fetch_date = date("Y-m-j G:i:s"); //NOTE: we don't use a DB SQL function so its database independant
+		$fetch_date = date("Y-m-j G:i:s"); //NOTE: we don't use a DB SQL function so its database independent
 		$item_title = $RSSitem->title;
 		$item_date  = date("Y-m-j G:i:s", strtotime($RSSitem->pubDate));
 		$item_url	= $RSSitem->link;
-
+	
 		echo "Processing item '" , $item_id , "' on " , $fetch_date 	, "<br/>";
 		echo $item_title, " - ";
 		echo $item_date, "<br/>";
 		echo $item_url, "<br/>";
-
+	
 		// Insert the item only if it does not exist already (based on hash key)
-		$item_exists_sql = "SELECT item_id FROM rssingest where item_id = '" . $item_id . "'";
-		$item_exists = mysql_query($item_exists_sql, $db);
-		if(mysql_num_rows($item_exists)<1)
+		$item_exists_sql = "SELECT item_id FROM " . $tbl_entries . " where item_id = '" . $item_id . "'";
+		echo "Exec " . $item_exists_sql ."<br/>";
+		//$item_exists = mysql_num_rows (mysql_query($item_exists_sql, $db));
+		if(mysqli_num_rows($mysqli->query($item_exists_sql)) >=1)
 		{
-			echo "<font color=green>Inserting new item..</font><br/>";
-			$item_insert_sql = "INSERT INTO rssingest(item_id, feed_url, item_title, item_date, item_url, fetch_date) VALUES ('" . $item_id . "', '" . $feed_url . "', '" . $item_title . "', '" . $item_date . "', '" . $item_url . "', '" . $fetch_date . "')";
-			$insert_item = mysql_query($item_insert_sql, $db);
-		}else{
 			echo "<font color=blue>Not inserting existing item..</font><br/>";
+		}else{
+			echo "<font color=green>Inserting new item..</font><br/>";
+			$item_insert_sql = "INSERT INTO " . $tbl_entries . " (item_id, feed_url, item_title, item_date, item_url, fetch_date) VALUES ('" . $item_id . "', '" . $feed_url . "', '" . $item_title . "', '" . $item_date . "', '" . $item_url . "', '" . $fetch_date . "')";
+			echo "Exec " . $item_insert_sql ."<br/>";
+			$res = $mysqli->query($item_insert_sql);
+			echo "Exec " . $res ."<br/>";
 		}
-
 		echo "<br/>";
-	}
-
-	//Select counter
-	$sql = "SELECT * FROM $tbl_name";
-	$result = mysql_query($sql);
-
-	$rows = mysql_fetch_array($result);
-	$counter = $rows['counter'];
-
-	// If there is not value in the counter, set it to 1
-	if(empty($counter)){
-		$counter = 1;
-		$sql1 = "INSERT INTO $tbl_name(counter) VALUES('$counter')";
-		$result1 = mysql_query($sql1);
-	}
-
-	// Increase the counter
-	$addcounter = $counter+1;
-	$sql2 = "UPDATE $tbl_name SET counter='$addcounter'";
-	$result2 = mysql_query($sql2);
-
-
-} catch (Exception $e)
-{
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
+	}	
+	
+	$mysqli->close();
+} catch (Exception $e) {
+	echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
 ?>
